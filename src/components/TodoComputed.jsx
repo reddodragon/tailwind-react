8c1209c38995d829eefa3cc7a8ddf88fda0719f9
Federico <federicoameratte@gmail.com>
2022-12-06 16:40:49 -0300

    a

--- src/components/TodoComputed.jsx
+++ src/components/TodoComputed.jsx
@@ -1,9 +1,9 @@
 
-const TodoComputed = ({computedItemsLeft, clearComplete}) => {
+const TodoComputed = ({computedItemsLeft, clearCompleted}) => {
   return (
     <section className="shadow-xl transition-all dration-1000 rounded-b-md py-4 px-4 flex justify-between bg-white dark:bg-gray-800">
         <span className="text-gray-400">{computedItemsLeft} items left</span>
-        <button onClick={clearComplete} className="text-gray-400">Clear Complete</button>
+        <button onClick={clearCompleted} className="text-gray-400">Clear Complete</button>
     </section>
   )
 }
--- src/components/TodoFilter.jsx
+++ src/components/TodoFilter.jsx
@@ -4,25 +4,25 @@ const TodoFilter = ({ changeFilter, filter }) => {
       <div className="shadow-xl flex justify-center gap-4 rounded-md bg-white p-4 dark:bg-gray-800 transition-all dration-1000">
         <button
           className={`${
-            filter === "All" ? " text-blue-600 hover:text-gray-400" : "text-gray-400 hover:text-blue-600"
+            filter === "all" ? " text-blue-600 hover:text-gray-400" : "text-gray-400 hover:text-blue-600"
           }`}
-          onClick={() => changeFilter("All")}
+          onClick={() => changeFilter("all")}
         >
           All
         </button>
         <button
           className={`${
-            filter === "Active" ? " text-blue-600 hover:text-gray-400" : "text-gray-400 hover:text-blue-600"
+            filter === "active" ? " text-blue-600 hover:text-gray-400" : "text-gray-400 hover:text-blue-600"
           }`}
-          onClick={() => changeFilter("Active")}
+          onClick={() => changeFilter("active")}
         >
           Active
         </button>
         <button
           className={`${
-            filter === "Completed" ? " text-blue-600 hover:text-gray-400" : "text-gray-400 hover:text-blue-600"
+            filter === "completed" ? " text-blue-600 hover:text-gray-400" : "text-gray-400 hover:text-blue-600"
           }`}
-          onClick={() => changeFilter("Completed")}
+          onClick={() => changeFilter("completed")}
         >
           Completed
         </button>
